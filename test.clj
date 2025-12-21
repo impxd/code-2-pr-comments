863eed5a30c1331ad7e1b918b5ff219997ab50ee
Marco Zambrano <marco.aguilar@nubank.com.br>
2025-12-20 19:30:26 -0600

    add: clojure test file

---
+++ test.clj
@@ -0,0 +1,15 @@
+(ns test)
+
+;; 💬 Single line test 1
+(println "Hello, World!")
+
+; 💬 Single line test 2
+(println "Hello, World!")
+
+;; 💬 Multiple lines test 2
+;; 2° line
+(println "Hello, World 2!")
+
+;; 💬 Multiple lines test 3
+;; 2° line
+(println "Goodbye, World!")
