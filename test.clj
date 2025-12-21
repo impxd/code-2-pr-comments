f6a4648310e822ab28a0ba8226fd5858908ee9a0
Marco Zambrano <marco.aguilar@nubank.com.br>
2025-12-20 19:34:49 -0600

    update: clojure test file

--- test.clj
+++ test.clj
@@ -6,10 +6,18 @@
 ; 💬 Single line test 2
 (println "Hello, World!")
 
-;; 💬 Multiple lines test 2
-;; 2° line
-(println "Hello, World 2!")
-
 ;; 💬 Multiple lines test 3
 ;; 2° line
-(println "Goodbye, World!")
+(println "Hello, World!")
+
+;; Single line test 4
+(println "Hello, World!")
+
+; Single line test 5
+(println "Hello, World!")
+
+;; Multiple lines test 6
+;; 2° line
+(println "Hello, World!")
+
+
